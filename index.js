3257d5a6fe9ab57e9432f277c6aa176ae6e9f647
oluwolebank <67108245+oluwolebank@users.noreply.github.com>
2020-07-01 13:40:49 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var friendList = ["Taiwo", "taiwo", "Opeyemi", "opeyemi", "tobiloba", 
+"Tobiloba", "Idowu", "idowu", "Kehinde", "kehinde", "Fadeke", "fadeke", 
+"Serah", "serah", "Comfort", "comfort", "Ayomikun", "ayomikun", "Banke", "banke", 
+"Ismail", "ismail", "Temitope", "temitope", "Dorcas", "dorcas", "Fatoye", "fatoye", 
+"Noah", "noah", "corlins", "Corlins", "Tosin", "tosin", "Oluwatosin", "oluwatosin", "Florence", "florence", 
+"Aguda", "aguda", "Femi", "femi", "Dayo", "dayo", "Shola", "shola", "Felix", "felix", 
+"Ranti", "ranti", "Bank", "bank", "Akinwalere", "akinwalere", "Oluwole", "oluwole",
+ "Oluyemi", "oluyemi"];
+var yourName = prompt("What is your name?");
+if (friendList.includes(yourName)) {
+    alert("Congratulations, " + yourName + "! You're a friend to Bank! keep being a good friend, He's such a nice dude!")
+}
+else alert("Oppsss! Sorry you're not on Bank's friend list, are you sure you're not a fake friend? if you're sure, kindly notify Bank on whatsapp number +2347033436566 to include you on his friend's list.")
+
